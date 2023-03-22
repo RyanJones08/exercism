69954573e81dd2616b689b4078f712f1a61cfcfb
Ryan Jones <ryan.jones@mx.com>
2023-03-22 16:08:07 -0600

    finish hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -7,6 +7,18 @@ To get started with TDD, see the `README.md` file in your
 =end
 class Hamming
   def self.compute(first_dna, second_dna)
-    
+    count = 0
+    i = 0
+    if (first_dna.length != second_dna.length)
+      raise ArgumentError.new("size mismatch")
+    end
+
+    while (i <= first_dna.length)
+      if (first_dna[i] != second_dna[i])
+        count += 1
+      end
+      i += 1
+    end
+    count
   end
 end
